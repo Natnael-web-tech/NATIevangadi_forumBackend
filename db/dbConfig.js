f8afe354ee42704227fa243a5b8ce0d959fbaa4d
Natnael-web-tech <gelalchanatnael@gmail.com>
2025-04-04 21:05:36 -0400

    dbConfig modification

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,9 +1,9 @@
 const mysql = require("mysql2");
 const db = mysql.createPool({
   host: "82.197.82.65",
-  user: "u435044055_evangadi_db",
+  user: "u435044055_evangadi_nati",
   password: "2025@Evangadi",
-  database: "u435044055_evangadi_nati",
+  database: "u435044055_evangadi_db",
 });
 
 module.exports = db.promise();
